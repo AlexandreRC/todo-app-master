1948a537c832331a2135cbd6986390cc3875a802
AlexandreRC <74365451+AlexandreRC@users.noreply.github.com>
2021-05-19 17:28:25 -0300

    Update Complete.js

--- src/components/Complete.js
+++ src/components/Complete.js
@@ -1,10 +1,7 @@
-import React, {useRef, useEffect, useState} from 'react';
-import Button from '../Button';
-
+import React, {useEffect, useState} from 'react';
 
 export default function Complete({todos, setTodos, changeCheck}) {  
     const trashIcon = process.env.PUBLIC_URL + './img/trash-2.svg';
-    const inputText = useRef();
     const [activeTodo, setActiveTodo] = useState([]);
     useEffect(() => {
         setActiveTodo(todos.filter(item => item.isChecked? true : false));
@@ -15,10 +12,6 @@ export default function Complete({todos, setTodos, changeCheck}) {
     }
     return (
         <>
-            <div className='insert-todo'>
-                <input placeholder='Add details' ref={inputText} type='text'/>
-                <Button onClick={() => setTodos([...todos , {name: inputText.current.value, isChecked: true}])} size='lg' color='#2F80ED'>Add</Button>
-            </div>
             <div className='todos-container'>
                 {activeTodo?.map(item => {
                 return (
